28c6036eeae3030cbb4412a71836724d9e19059a
Sy Rashid <sy.rashid.11@gmail.com>
2022-05-17 10:15:58 +0100

    NOTE: Add notes on routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Create, Read, Update, Delete
+  # See all restaurants
+  # See one restaurant
+  # Create a restaurant
+  # Update a restaurant
+  # Destroy a restaurant
 end
