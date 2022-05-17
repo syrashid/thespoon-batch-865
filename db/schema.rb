0eb052d6f2c362dc379496f2dcec693275a59fe8
Sy Rashid <sy.rashid.11@gmail.com>
2022-05-17 10:02:05 +0100

    ARCH: Modify restaurants table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_17_083212) do
+ActiveRecord::Schema.define(version: 2022_05_17_084416) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.integer "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "address"
   end
 
 end
